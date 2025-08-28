0401f93891439732933da08a02e473eab93b73c9
Gaurav Jadhav <gauravmakarandjadhav@gmail.com>
2025-08-28 11:52:43 +0530

    Updating...
    Starting with the concepts of OOP using C++ Programming Language

--- OOP/program3.cpp
+++ OOP/program3.cpp
@@ -1,48 +1,36 @@
 #include<iostream>
 using namespace std;
 
-class Demo
-{
-    int i;
-    public:
-        int j;
-    private:
-        int k;
-    protected:
-        int a;
-
-
-    public:
-        Demo()
-        {
-            i = 10;
-            j = 20;
-            k = 30;
-            a = 40;
-        }
-
-        void display()
-        {
-            cout<<"Value of i :: "<<i<<endl;
-            cout<<"Value of j :: "<<j<<endl; 
-            cout<<"Value of k :: "<<k<<endl;
-            cout<<"Value of a :: "<<a<<endl;
-        }
-};
-
-class Hello : public Demo{
-    public:
-        void HelloDisplay()
-        {
-            cout<<"Value of j :: "<<j<<endl;
-            cout<<"Value of a :: "<<a<<endl;
-        }
-};
-
 int main()
 {
-    Hello hobj;
-    hobj.display();
-    hobj.HelloDisplay();
+    int no = 11;
+    int &noref = no;
+
+    int gojo = 26;
+    int &gojoref = gojo;
+
+    int *ptr = &no;
+    int *(&ptrRef) = ptr;
+
+    cout<<"no :: "<<no<<endl; // 11
+    cout<<"noref :: "<<noref<<endl; // 11
+
+    cout<<"&no :: "<<&no<<endl; // Address = 100
+    cout<<"&noref :: "<<&noref<<endl; // Address = 100
+
+    cout<<"*ptr :: "<<*ptr<<endl; // 11
+    cout<<"*ptrRef :: "<<*ptrRef<<endl; // 11
+
+    no++;
+    cout<<"after incrementing no"<<endl;
+    cout<<"no :: "<<no<<endl; // 12
+    cout<<"noref :: "<<noref<<endl; // 12
+
+    noref++;
+    cout<<"after incrementing noref"<<endl;
+    cout<<"no :: "<<no<<endl; // 13
+    cout<<"noref :: "<<noref<<endl; // 13   
+
+
     return 0;
 }
