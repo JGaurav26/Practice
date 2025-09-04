9843909262a0f6154d1eed6b2c0150c1d0954e41
Gaurav Jadhav <gauravmakarandjadhav@gmail.com>
2025-09-04 23:11:36 +0530

    Updating
    Completed the Binary Function using iterative method

---
+++ College/DSA/Theory/Unit 2/BinarySearch1.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void BinarySearch()
+{
+    int n=0;
+    printf("Enter the total number of elements in Array :: ");
+    scanf("%d", &n);
+
+    int *iArr = (int *)malloc(n * sizeof(int));
+    printf("Enter the %d elements in sorted order:: ", n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &iArr[i]);
+    }
+    printf("Sorted Array Elements -> ");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", iArr[i]);
+    }
+    printf("\n");
+    int key = 0;
+    printf("Enter the element to be searched :: ");
+    scanf("%d", &key);
+
+    // LOGIC
+    int low, mid, high;
+    low = 0, high = n-1;
+
+    while(low<=high)
+    {
+        mid = (low + high)/2;
+        if(iArr[mid] == key)
+        {
+            printf("%d found at the Index %d \n", key, mid);
+            free(iArr);
+            return;
+        }
+        if(iArr[mid]<key)
+        {
+            low = mid + 1;
+        } else{
+            high = mid - 1;
+        }
+    }
+    printf("%d not found in the given array\n", key);
+}
+
+int main()
+{
+    BinarySearch();
+    return 0;
+}
